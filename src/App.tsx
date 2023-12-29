8cea1756ea57149b1fb0a000e8455d0af6dd2aee
almoxarife <logistica@aventuradesign.com.br>
2023-12-29 16:10:17 -0400

    feat: fil reader function

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import logo from './bglogo.jpg';
 import './App.css';
+import * as fs from "fs";
 
 function App() {
+  const [file, setFile] = useState("")
+  const readFile = (e: any) => {
+    const csv = e.target.files[0]
+
+    if (!csv) {
+      return;
+    }
+
+    const fileReader = new FileReader()
+
+    fileReader.readAsText(csv)
+    fileReader.onload = () => {
+      let lines = JSON.stringify(fileReader.result).replace(/["]+/g, "").split("\\r\\n").filter((item) => item.length !== 0)
+      console.log(lines)
+      setFile(JSON.stringify(fileReader.result))
+    }
+    fileReader.onerror = () => {
+      console.log(fileReader.error)
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="file" name="file" id='file' onChange={readFile} />
     </div>
   );
 }
